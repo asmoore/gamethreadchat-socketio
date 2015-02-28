f81489a25d70a7fa573f4a429f12a3e5d48f3859
Andrew Moore <asmoor@gmail.com>
2015-02-28 08:53:17 -0500

    add looped ping

--- chat.py
+++ chat.py
@@ -14,5 +14,11 @@ def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
     socketio.emit('echo', {'echo': 'Server Says: '+text})
 
+@app.route('/echo')
+def ping():
+	while True:
+		time.sleep(5)
+	    socketio.emit('echo', {'echo': 'Server Says: test!'})
+
 if __name__ == '__main__':
     socketio.run(app)
