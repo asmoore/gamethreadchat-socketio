2f8ab6abba8c15042e57d1e7724d6998cd185ff7
Andrew Moore <asmoor@gmail.com>
2015-02-28 10:18:45 -0500

    pretty sure indentations are trying to ruin my day

--- chat.py
+++ chat.py
@@ -19,15 +19,6 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/chat')
-def chat():
-	global thread
-	if thread is None:
-		thread = Thread(target=ping)
-		thread.start()
-	return render_template('chat.html')
-
-
 @socketio.on('send_message')
 def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
@@ -37,13 +28,13 @@ def ping():
 	while True:
 		time.sleep(5)
 		comment_dict = {"author": "catmoon", 
-                        "body": "test", 
-                        "author_flair_css_class": "Heat1", 
-                        "comment_id": "qwresdf", 
-                        "score": "sdfsafd",
-                        "created_utc": "123123123", 
-                        "emitted": "true"}
-        message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
+						"body": "test", 
+						"author_flair_css_class": "Heat1", 
+						"comment_id": "qwresdf", 
+						"score": "sdfsafd",
+						"created_utc": "123123123", 
+						"emitted": "true"}
+		message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
 		socketio.emit('echo', message)
 
 if __name__ == '__main__':
