df2e9507912c545d039cb84d6967147f60f8f8c5
Andrew Moore <asmoor@gmail.com>
2015-02-28 09:14:05 -0500

    add thread to app

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 import time
+from threading import Thread
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -6,9 +7,14 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+thread = None
 
 @app.route('/')
 def index():
+	global thread
+    if thread is None:
+        thread = Thread(target=ping)
+        thread.start()
     return render_template('index.html')
 
 @socketio.on('send_message')
@@ -16,11 +22,10 @@ def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
     socketio.emit('echo', {'echo': 'Server Says: '+text})
 
-@app.route('/echo')
 def ping():
 	while True:
 		time.sleep(5)
-		socketio.emit('echo', {'echo': 'Server Says: test!'}, namespace='/echo')
+		socketio.emit('echo', {'echo': 'Server Says: test!'})
 
 if __name__ == '__main__':
     socketio.run(app)
