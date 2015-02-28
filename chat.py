c29f33946281922afac257cafb96677b42b8d678
Andrew Moore <asmoor@gmail.com>
2015-02-28 10:11:47 -0500

    add chat.html

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 import time
 from threading import Thread
+import json
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
@@ -17,6 +18,16 @@ def index():
 		thread.start()
 	return render_template('index.html')
 
+
+@app.route('/chat')
+def index():
+	global thread
+	if thread is None:
+		thread = Thread(target=ping)
+		thread.start()
+	return render_template('chat.html')
+
+
 @socketio.on('send_message')
 def handle_source(json_data):
     text = json_data['message'].encode('ascii', 'ignore')
@@ -25,7 +36,15 @@ def handle_source(json_data):
 def ping():
 	while True:
 		time.sleep(5)
-		socketio.emit('echo', {'echo': 'Server Says: test!'})
+		comment_dict = {"author": "catmoon", 
+                        "body": "test", 
+                        "author_flair_css_class": "Heat1", 
+                        "comment_id": "qwresdf", 
+                        "score": "sdfsafd",
+                        "created_utc": "123123123", 
+                        "emitted": "true"}
+        message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
+		socketio.emit('echo', message)
 
 if __name__ == '__main__':
     socketio.run(app)
